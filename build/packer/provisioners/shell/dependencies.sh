24c54eeba71a970d73eb176360c8dcfb7cdc1552
Dean Kayton <dean.kayton@opendna.ai>
2017-10-20 19:35:26 +0200

    Refactor

--- build/packer/provisioners/shell/dependencies.sh
+++ build/packer/provisioners/shell/dependencies.sh
@@ -5,7 +5,7 @@ apk --no-cache update && apk --no-cache upgrade
 apk --no-cache --update add virtualbox-guest-additions virtualbox-guest-modules-virthardened
 rc-update add virtualbox-guest-additions
 echo vboxsf >> /etc/modules
-# echo vboxguest >> /etc/modules
+echo vboxguest >> /etc/modules
 apk --no-cache --update add ca-certificates openssl wget docker
 update-ca-certificates
 wget https://releases.hashicorp.com/packer/1.1.1/packer_1.1.1_linux_amd64.zip; unzip packer_1.1.1_linux_amd64.zip -d /usr/local/bin; rm packer_1.1.1_linux_amd64.zip
